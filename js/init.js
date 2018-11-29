8d31acb358cb3cb525a66a774f606b06513de57b
unknown <vaseraja888@gmail.com>
2018-11-29 18:26:31 +0530

    home contact updated

--- js/init.js
+++ js/init.js
@@ -25,7 +25,7 @@ jQuery(function($) {
 	$('.tile-progress .tile-header').matchHeight();
 
 	var footerHeight = jQuery('#footer-wrapper').outerHeight();
-	jQuery('#content-wrapper').css('margin-bottom', footerHeight );
+	// jQuery('#content-wrapper').css('margin-bottom', footerHeight );
 
 	var windowsHeight = jQuery(window).height();
 	var navHeight = jQuery('navbar-fixed-top').outerHeight();
